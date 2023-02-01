3a8d577510c04d4452917153b5d9535c25ef4424
Adrian Felipe Ceron <afecego@gmail.com>
2023-02-01 14:36:30 -0500

    Excercice whit flask

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+@app.errorhandler(404)
+def pag_not_found(error):
+    return render_template('error404.html', error=error), 404
+
+@app.route('/numeros/<int:numeros>')
+def numeros(numeros):
+    num = str(numeros)
+    num = ''.join(sorted(num))
+    result = int(num)
+    return jsonify(f'Numeros ordenados de menor a mayor: {result}')
+
+@app.route('/libro/<libro>')
+def libro(libro):
+    try:
+        with open(f'{libro}.txt', 'r', encoding='utf-8') as libro_file:
+            data = libro_file.read()
+        counter = 0
+        for i in data:
+            if i.isupper():
+                counter += 1
+        return jsonify(f'Cantidad de letras mayusculas: {counter}')
+    except FileNotFoundError:
+        message = f'Libro "{libro}" no fue encontrado.'
+        return(message)
+
